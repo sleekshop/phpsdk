1f0e1bee991a0cfa0c84802f4f33b886229c89c0
Balamurali Pandranki <balamurali@live.com>
2018-11-25 13:24:25 +0530

    Add Composer & PSR Namespace

---
+++ src/session.inc.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Sleekshop;
+
+class SessionCtl
+{
+
+  function __construct()
+  {
+
+  }
+
+/*
+ * Delivers a valid session and returns it
+ */
+public static function GetSession()
+ {
+  $sr=new SleekShopRequest();
+  $xml=$sr->get_new_session();
+  $xml=new SimpleXMLElement($xml);
+  $code=(string)$xml->code;
+  return($code);
+}
+
+
+}
+
+?>
